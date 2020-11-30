977379974a65db90e349df630e8ccec3a787a5bf
Abhishek Singh <writetoabhishek07@gmail.com>
2020-12-01 00:02:04 +0530

    started developing Chat component and added routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Chat from './Chat';
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
-      <h1>Let's build the Slack Clone</h1>
       {/*Header */}
+      <Router>
+         <Header/>
+         <div className="app__body">
 
-      <Header/>
       {/*Sidebar */}
       <Sidebar/>
-
+      <Switch>
+        <Route path = "/room/:roomId">
+          <Chat/>
+        </Route>
+        <Route path = "/">
+          <h1>Welcome</h1>
+        </Route>
+      </Switch>
       {/*React Router -> Chat Screen */}
+         </div>
+      </Router>
     </div>
   );
 }

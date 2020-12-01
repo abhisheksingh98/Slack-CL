9b1ec0080906b0320452c794b0b15dc1d78a15d2
Abhishek Singh <writetoabhishek07@gmail.com>
2020-12-02 02:13:44 +0530

    modified chat and chatInput

--- src/ChatInput.js
+++ src/ChatInput.js
@@ -1,46 +1,42 @@
-import React, { useState } from 'react'
-import db from './firebase';
-import firebase from './firebase';
-import { useStateValue } from './StateProvider';
-import './ChatInput.css'
+import React, { useState } from "react";
+import db from "./firebase";
+import "./ChatInput.css";
+import { useStateValue } from "./StateProvider";
+import firebase from "firebase";
 
-function ChatInput({channelName, channelId}) {
+function ChatInput({ channelName, channelId }) {
+  const [input, setInput] = useState("");
+  const [{ user }] = useStateValue();
 
-    const [input, setInput] = useState('');
-    const [{user}] = useStateValue();
+  const sendMessage = (e) => {
+    e.preventDefault();
 
-    const sendMessage = e => {
-        e.preventDefault();
-
-        // if(channelId){
-        //     db.collection('rooms').doc(channelId).collection("messages").add({
-        //         message: input,
-        //         timestamp: firebase.firestore.FieldValue.server(),
-        //         user: user.displayName,
-        //         userImage: user.photoURL,
-        //     })
-        // }
-        if (channelId) {
-            db.collection("rooms").doc(channelId).collection("messages").add({
-              message: input,
-              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-              user: user.displayName,
-              userImage: user.photoURL,
-            });
-          }
+    if (channelId) {
+      db.collection("rooms").doc(channelId).collection("messages").add({
+        message: input,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        user: user.displayName,
+        userImage: user.photoURL,
+      });
     }
 
-    return (
-        <div className="chatInput">
-            <form>
-                <input
-                value={input}
-                onChange = {e=>setInput(e.target.value)}
-                placeholder={`Message #${channelName?.toLowerCase()}`} />
-                <button type="submit" onClick={sendMessage} >SEND</button>
-            </form>
-        </div>
-    )
+    setInput("");
+  };
+
+  return (
+    <div className="chatInput">
+      <form>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder={`Message # ${channelName?.toLowerCase()}`}
+        />
+        <button type="submit" onClick={sendMessage}>
+          SEND
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default ChatInput
+export default ChatInput;
